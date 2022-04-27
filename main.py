f1a46a05a2da03be255f94fbc80191ea30f9826b
AndrewBuryachok <58564970+AndrewBuryachok@users.noreply.github.com>
2022-04-27 20:51:33 +0300

    add Legendre, add all alg, start part 2

--- main.py
+++ main.py
@@ -1,10 +1,78 @@
 import math
+from random import randint
 from itertools import chain, combinations
+
+def quick_pow(a, b, m):
+    ab = 1
+    while b > 0:
+        if b & 1:
+            ab = (ab * a) % m
+        b >>= 1
+        a = (a ** 2) % m
+    return ab
+
+def get_ds(p):
+    p -= 1
+    s = 0
+    while p % 2 == 0:
+        p >>= 1
+        s += 1
+    return (p, s)
+
+def strongly_prime(p, x, d, s):
+    xd = quick_pow(x, d, p)
+    if xd == 1 or xd == p - 1:
+        return True
+    xr = xd
+    for r in range(1, s):
+        xr = quick_pow(xr, 2, p)
+        if xr == p - 1:
+            return True
+        if xr == 1:
+            return False
+
+def Millera_Rabina(p):
+    d, s = get_ds(p)
+    if not(strongly_prime(p, 2, d, s)) or not(strongly_prime(p, 3, d, s)) or not(strongly_prime(p, 5, d, s)) or not(strongly_prime(p, 7, d, s)):
+        return False
+    k = 0
+    while k < 5:
+        x = randint(2, p - 1)
+        if math.gcd(x, p) > 1 or not(strongly_prime(p, x, d, s)):
+            return False
+        k += 1
+    return True
+
+#
+
+def trial_divisions(num):
+    for d in range(2, num ** 0.5):
+        if num % d == 0:
+            return d
+
+#
+
+def f(x):
+    return (x ** 2 + 1) % n
+
+def Pollard():
+    for x in range(2, n):
+        y = x
+        while True:
+            x = f(x)
+            y = f(f(y))
+            d = math.gcd(x - y)
+            if x == y:
+                break
+            if d != 1:
+                return d
+
+#
 
 n = 25511
 sn = n ** 0.5
+La = math.e ** ((1 / 2 ** 0.5) * (math.log2(n) * math.log2(math.log2(n))) ** 0.5)
 
-# init
 V = [1]
 Al = [sn]
 A = [math.floor(Al[0])]
@@ -34,17 +102,30 @@ def table2():
     print("b =", B[-1])
     print("b2 =", B2[-1])
 
+def Legendre_symbol(a, b):
+    return pow(a, int((b - 1) / 2), b)
+    '''if a == 1:
+        return 1
+    if a % 2 == 0:
+        return Legendre_symbol(a / 2, b) * ((-1) ** ((b * b - 1) / 8))
+    return Legendre_symbol(b % a, a) * ((-1) ** ((a - 1) * (b - 1) / 4))'''
+
+def check_divisor(p):
+    return Legendre_symbol(n, p) == 1 and p < La
+
 def prime_divisors(num):
     P.clear()
     if num < 0:
         P[-1] = 1
         num *= -1
-        Fb[-1] = 1
+        #Fb[-1] = 1
     p = 2
     while pow(p, 2) <= num:
         if num % p == 0:
-            Fb[p] = 1
+            #Fb[p] = 1
             P[p] = 1
+            if not check_divisor(p):
+                return False
             num /= p
             while num % p == 0:
                 P[p] = 1 - P[p]
@@ -53,9 +134,14 @@ def prime_divisors(num):
     if num > 1:
         num = int(num)
         P[num] = 1
-        Fb[num] = 1
+        if not check_divisor(p):
+            return False
+        #Fb[num] = 1
+    for p in P:
+        Fb[p] = 1
     print("P =", P)
     print("Fb =", list(Fb))
+    return True
 
 def build_vectors():
     print("new len =", len(Fb), "last len =", vec_len)
@@ -122,11 +208,21 @@ def Brilhart_Morrison():
         if i != 1:
             table1()
         table2()
-        prime_divisors(B2[-1])
+        if not prime_divisors(B2[-1]):
+            continue
         build_vectors()
         if solve_vectors():
             break
         i += 1
         print()
 
-Brilhart_Morrison()
+#
+
+def function_a(num):
+    return Millera_Rabina(num)
+
+def function_b():
+    pass
+
+def hell_part():
+    pass
