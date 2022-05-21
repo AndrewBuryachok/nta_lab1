43c1860eb1c4b5cd0c56f5f0bcfa98e657277d44
AndrewBuryachok <58564970+AndrewBuryachok@users.noreply.github.com>
2022-05-21 18:03:08 +0300

    add task 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 from random import randint
 from itertools import chain, combinations
+import time
 
 def quick_pow(a, b, m):
     ab = 1
@@ -46,44 +47,75 @@ def Millera_Rabina(p):
 #
 
 def trial_divisions(num):
-    for d in range(2, num ** 0.5):
+    for d in range(2, int(num ** 0.5)):
+        #if d >= 47:
+            #break
         if num % d == 0:
             return d
+    return -1
 
 #
 
-def f(x):
-    return (x ** 2 + 1) % n
+def f(x, num):
+    return (x ** 2 + 1) % num
 
-def Pollard():
-    for x in range(2, n):
+def Pollard(num):
+    for x in range(2, num):
         y = x
         while True:
-            x = f(x)
-            y = f(f(y))
-            d = math.gcd(x - y)
+            x = f(x, num)
+            y = f(f(y, num), num)
+            d = math.gcd(x - y, num)
             if x == y:
                 break
             if d != 1:
                 return d
+    return -1
 
 #
 
-n = 25511
-sn = n ** 0.5
-La = math.e ** ((1 / 2 ** 0.5) * (math.log2(n) * math.log2(math.log2(n))) ** 0.5)
+n = 1
+sn = 1
+a = 1
+La = 1
 
-V = [1]
-Al = [sn]
-A = [math.floor(Al[0])]
-U = [A[0]]
-B = [0, 1]
-B2 = [0, 1]
+V = []
+Al = []
+A = []
+U = []
+B = []
+B2 = []
 P = {}
 Fb = {}
 Vec_b = {}
 vec_len = 0
 Vec_all = []
+
+def calc_La():
+    global La
+    La = math.e ** (a * (math.log2(n) * math.log2(math.log2(n))) ** 0.5)
+    print("a =", a)
+    print("La =", La)
+
+def init_Brilhart_Morrison(num):
+    global n, sn, a, V, Al, A, U, B, B2, P, Fb, Vec_b, vec_len, Vec_all
+    n = num
+    sn = n ** 0.5
+    a = (1 / 2) ** 0.5
+
+    calc_La()
+
+    V = [1]
+    Al = [sn]
+    A = [math.floor(Al[0])]
+    U = [A[0]]
+    B = [0, 1]
+    B2 = [0, 1]
+    P = {}
+    Fb = {}
+    Vec_b = {}
+    vec_len = 0
+    Vec_all = []
 
 def table1():
     V.append((n - U[-1] ** 2) / V[-1])
@@ -102,16 +134,38 @@ def table2():
     print("b =", B[-1])
     print("b2 =", B2[-1])
 
-def Legendre_symbol(a, b):
-    return pow(a, int((b - 1) / 2), b)
-    '''if a == 1:
+def Legendre_symbol(a,p):
+  if(a==1):
+    return 1
+  if(a%p==0):
+    return 0
+  if(a%2==0):
+    return Legendre_symbol(a/2,p)*pow(-1,(pow(p,2)-1)/8)
+  elif(a%2!=0 and a!=1):
+    return Legendre_symbol(pow(int(p),1,int(a)),a)*pow(-1,((a-1)*(p-1))/4)
+
+'''def Legendre_symbol(a, b):
+    #return 1
+    #return pow(a, int((b - 1) / 2), b)
+    if a == 1:
         return 1
+    if a % b == 0:
+        return 0
     if a % 2 == 0:
-        return Legendre_symbol(a / 2, b) * ((-1) ** ((b * b - 1) / 8))
-    return Legendre_symbol(b % a, a) * ((-1) ** ((a - 1) * (b - 1) / 4))'''
+        return Legendre_symbol(a / 2, b) * pow(-1, (pow(b, 2) - 1) / 8)
+        #return Legendre_symbol(a / 2, b) * ((-1) ** ((b * b - 1) / 8))
+    #return Legendre_symbol(b % a, a) * ((-1) ** ((a - 1) * (b - 1) / 4))
+    return Legendre_symbol(b % a, a) * pow(-1, ((a - 1) * (b - 1)) / 4)'''
 
 def check_divisor(p):
-    return Legendre_symbol(n, p) == 1 and p < La
+    #return True
+    ls = Legendre_symbol(n, p)
+    if ls != 1:
+        print("Legendre_symbol failed", n, p)
+    if p >= La:
+        print("La failed")
+    return ls == 1 and p < La
+    #return 1
 
 def prime_divisors(num):
     P.clear()
@@ -193,13 +247,16 @@ def solve_vectors():
                 d2 = math.gcd(X - Y, n)
                 if d1 != 1 and d2 != 1:
                     print(d1, d2)
-                    return True
+                    return (d1, d2)
                 print("gcd = 1")
+                global a
+                a += 1
             else:
                 print("X = +- Y")
-    return False
+    return (-1, -1)
 
-def Brilhart_Morrison():
+def Brilhart_Morrison(num):
+    init_Brilhart_Morrison(num)
     i = 1
     while True:
         print("i =", i)
@@ -208,21 +265,83 @@ def Brilhart_Morrison():
         if i != 1:
             table1()
         table2()
+        i += 1
         if not prime_divisors(B2[-1]):
             continue
         build_vectors()
-        if solve_vectors():
-            break
-        i += 1
+        res = solve_vectors()
+        if res != (-1, -1):
+            return res
         print()
+    return (-1, -1)
 
 #
 
-def function_a(num):
-    return Millera_Rabina(num)
+def main_part(number):
+    number_before = number
+    result = []
+    state = 1
+    while True:
+        if state % 2 == 1:
+            print("stage =", state)
+            if Millera_Rabina(number):
+                print(number, "prime")
+                result.append(number)
+                break
+            else:
+                print(number, "not prime")
+                state += 1
+        if state == 2:
+            print("stage =", state)
+            d = trial_divisions(number)
+            print("d =", d)
+            if d != -1:
+                result.append(d)
+                state = 1
+                number = int(number / d)
+            else:
+                state = 4
+        if state == 4:
+            print("stage =", state)
+            d = Pollard(number)
+            print("d =", d)
+            if d != -1:
+                result.append(d)
+                state = 3
+                number = int(number / d)
+            else:
+                state = 6
+        if state == 6:
+            print("stage =", state)
+            d = Brilhart_Morrison(number)
+            print("d =", d)
+            if d != (-1, -1):
+                result.append(d[0])
+                state = 5
+                number = int(number / d[0])
+            else:
+                print("povnuy kopes")
+    print(result)
+    res = 1
+    for r in result:
+        res *= r
+    print(res, number_before == res)
 
-def function_b():
-    pass
+#main_part(49347803087)
 
-def hell_part():
-    pass
+#
+
+results = []
+
+def measure_time(num):
+    start = time.perf_counter()
+    print("Pollard", Pollard(num))
+    end = time.perf_counter()
+    print(start, end, end - start)
+
+values = [9172639163, 8627969789, 8937716743, 278874899, 99400891, 116381389, 4252083239, 6633776623, 227349247, 3568572617]
+
+for r in values:
+    measure_time(r)
+
+#Brilhart_Morrison(10967535067)
